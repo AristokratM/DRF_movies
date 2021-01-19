a27bf99214a283b4634782fa71f302c14c450d37
AristokratM <61250102+AristokratM@users.noreply.github.com>
2021-01-20 00:03:42 +0200

    Review representation change

--- movies/models.py
+++ movies/models.py
@@ -133,8 +133,10 @@ class Review(models.Model):
     email = models.EmailField()
     name = models.CharField("Ім'я", max_length=100)
     text = models.TextField("Повідомлення", max_length=5000)
-    parent = models.ForeignKey('self', verbose_name="Батько", on_delete=models.SET_NULL, blank=True, null=True)
-    movie = models.ForeignKey(Movie, verbose_name="Фільм", on_delete=models.CASCADE, related_name="reviews")
+    parent = models.ForeignKey(
+        'self', verbose_name="Батько", on_delete=models.SET_NULL, blank=True, null=True, related_name="children"
+    )
+    movie = models.ForeignKey(Movie, verbose_name="Фільм", on_delete=models.CASCADE, related_name='reviews')
 
     def __str__(self):
         return f"{self.name} - {self.movie}"
--- movies/serializers.py
+++ movies/serializers.py
@@ -10,12 +10,30 @@ class ReviewCreateSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
+class FilterReviewSerializer(serializers.ListSerializer):
+    """Фільтр відгуків, тільки parents"""
+
+    def to_representation(self, data):
+        data = data.filter(parent=None)
+        return super().to_representation(data)
+
+
+class RecursiveSerializer(serializers.Serializer):
+    """Вивід рекурсивно children"""
+
+    def to_representation(self, value):
+        serializer = self.parent.parent.__class__(value, context=self.context)
+        return serializer.data
+
+
 class ReviewSerializer(serializers.ModelSerializer):
     """Вивід відгуків"""
+    children = RecursiveSerializer(many=True)
 
     class Meta:
+        list_serializer_class = FilterReviewSerializer
         model = Review
-        fields = ('name', 'text', 'parent')
+        fields = ('name', 'text', 'children')
 
 
 class MovieListSerializer(serializers.ModelSerializer):
@@ -23,7 +41,7 @@ class MovieListSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Movie
-        fields = ('title', 'tagline', 'category')
+        fields = ('title', 'tagline', 'category',)
 
 
 class MovieDetailSerializer(serializers.ModelSerializer):
